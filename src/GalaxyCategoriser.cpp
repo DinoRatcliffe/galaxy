59b8a90a022293739ab361672af75de3849062c1
Dino Ratcliffe <yo@dino.io>
2013-10-30 13:21:49 +0000

    Same as master but now more oop

--- src/GalaxyCategoriser.cpp
+++ src/GalaxyCategoriser.cpp
@@ -1,13 +1,45 @@
 #include "GalaxyCategoriser.h"
+#include <vector>
+#include <imgproc/imgproc.hpp>
 
 GalaxyCategoriser::GalaxyCategoriser() {
+	vib_upper	= 255;
+	vib_lower	= 50;
+	sat_upper	= 255;
+	sat_lower	= 10;
+	hue_upper	= 130;
+	hue_lower	= 90;
 
+	thresh		= 250;
 }
 
 GalaxyCategoriser::GalaxyType GalaxyCategoriser::categoriseGalaxy(cv::Mat image) {
-	return SPIRAL;
+	cv::Mat imageHsv;
+	cvtColor(image, imageHsv, CV_BGR2HSV);
+	
+	int pixels = countPixels(imageHsv);
+	return pixels > thresh ? SPIRAL : ELLIPSE;
 }
 
 int GalaxyCategoriser::countPixels(cv::Mat image) {
-	return -1;
+	std::vector<cv::Mat> hsvChannels;
+	split(image, hsvChannels);
+
+	int numPix = 0;
+	for (int i = 0; i < image.rows; i++) {
+		for (int j = 0; j < image.cols; j++) {
+			int hue = (int) hsvChannels[0].at<uchar>(i, j);
+			int sat = (int) hsvChannels[1].at<uchar>(i, j);
+			int vib = (int) hsvChannels[2].at<uchar>(i, j);
+
+			if (	hue > hue_lower && hue < hue_upper &&
+				sat > sat_lower && sat < sat_upper &&
+				vib > vib_lower && vib < vib_upper
+			) {
+				numPix++;
+			}
+		}	
+	}	
+
+	return numPix;
 }
