496c0e5095bdf44d87b9a967e76c01e4da865bee
Dino Ratcliffe <yo@dino.io>
2013-11-04 10:33:02 +0000

    Cleanup

--- src/galaxy.cpp
+++ src/galaxy.cpp
@@ -26,17 +26,64 @@
 #include <core/core.hpp>
 #include <highgui/highgui.hpp>
 #include "GalaxyCategoriser.h"
+#include <unistd.h>
+
+////////////////////////////////////////////////////////////////////
+// Option handling
+////////////////////////////////////////////////////////////////////
+void printUsage(char* program) {
+	printf("usage: %s [ -h ] image\n", program);
+
+}
+
+int handleOptions(int argc, char** argv) {
+	bool needUsage = false;
+	//handle flags
+	int c;
+	while ((c = getopt(argc, argv, "h")) != -1){
+		switch (c) {
+			case 'h':
+				needUsage = true;
+			       	break;	
+			case '?':
+				 if (isprint (optopt)) fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				 else fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+				 needUsage = true;
+				 break;
+		}
+	}
+	
+	//make sure there is a non flag argument
+	if ((argc - optind) == 0) {
+	       	printf("Requires image argument\n");
+		needUsage = true;
+	}
+
+	if (needUsage) {
+		printUsage(argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	return optind;
+}
 
 ////////////////////////////////////////////////////////////////////
 // Main
 ////////////////////////////////////////////////////////////////////
 int main(int argc, char** argv) {
 
+	int index = handleOptions(argc, argv);	
+
 	//grab image
-	char* imageName = argv[1];
+	char* imageName = argv[index];
 	cv::Mat image, imageHSV;
 	image = cv::imread( imageName, CV_LOAD_IMAGE_COLOR );
 
+	if (! image.data) { 
+		printf("Error - Cannot open image \"%s\"\n", argv[index]);
+		exit(EXIT_FAILURE);
+	}
+	
 	//get and print type
 	GalaxyCategoriser cat;
 	if (cat.categoriseGalaxy(image) == GalaxyCategoriser::SPIRAL) {
